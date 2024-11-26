c205e2798cc060062bb600d86480029620702d0e
Stin9469 <Stinger1994@mail.ru>
2024-11-26 17:44:58 +0300

    bootstrap - настройка

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import img from './resources/img/backGround.jpg';
+
 
 function App() {
-  return (
+/*   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +25,21 @@ function App() {
         </a>
       </header>
     </div>
-  );
+ ); */
+
+
+return (
+<Container>
+<Row>
+  <Col xs={12} md={6} xxl={6}> 
+    <Image className='Img'  src={img} roundedCircle />
+  </Col>
+  <Col xs={12} md={6} xxl={6}> 
+    <Image className='Img' src={img} roundedCircle />
+  </Col>
+</Row>
+</Container>
+);
 }
 
 export default App;
