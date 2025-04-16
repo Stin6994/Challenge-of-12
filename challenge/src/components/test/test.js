b3c315e34150631aa7eace1c5aa22908dcb25e76
Stin9469 <Stinger1994@mail.ru>
2025-04-16 14:44:38 +0300

    state с кнопкой и change работает как надо

--- challenge/src/components/test/test.js
+++ challenge/src/components/test/test.js
@@ -1,61 +1,82 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 
 
-function useInputWithValidate(initialValue) {
+/* function useInputWithValidate(initialValue) {
     const [value, setValue] = useState(initialValue);
 
     const onChange = event => {
+          event.preventDefault();
         setValue(event.target.value);
     }
 
-    const plus = event =>  {
+    const plus = event => {
         event.preventDefault();
+  
+        console.log(event.target.value);
         setValue(event.target.value++);
-        /* area.value++; */
-        /* console.log(input.value, area.value); */
+        console.log(event.target.value);
     }
 
-    return { value, onChange, plus }  // равнозначно {value: value, onChange: onChange}
+    return { value, onChange, plus }  
 }
+ */
+
 
 const FormMyHooks = () => {
 
+    const [value, setValue] = useState(5);
 
-    const input = useInputWithValidate('');
-    const area = useInputWithValidate(2);
 
-    
+
+
+    function onChange(event) {
+        event.preventDefault();
+        setValue(event.target.value);
+    }
+
+/*     useEffect(() => {
+        
+      }); */
+
+/*       function plus (event) {
+        event.preventDefault();
+        setValue((value) => value + 1);
+    } */
+
+    function plus (event) {
+        event.preventDefault();
+        setValue((value) => Number(value) + 1);
+    }
+
+
+    /* const area = useInputWithValidate(''); */
 
     return (
-        <Fragment>
+        <div>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input value={`${input.value} / ${area.value}`} type="text" className="form-control" readOnly />
-                    <label className="form-label mt-3">Email address</label>
-                    <input
-                        onChange={input.onChange}
-                        /* type="email" */
-                        value={input.value}
-                        placeholder="name@example.com" />
+                    <input value={value * 2} readOnly />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea
-                        onChange={area.onChange}
-                        value={area.value}
+                    <input
+                        onChange={onChange}
+                        value={value}
                         className="form-control"
                         id="exampleFormControlTextarea1"
-                        rows="3"></textarea>
+                        rows="3"
+                        type="number">
+                            
+                        </input>
                 </div>
                 <div>
                     <button
-                        onClick={input.plus}
-                        >plus
-                        
+                        onClick={plus}
+                    >plus
                     </button>
                 </div>
             </form>
-        </Fragment>
+        </div>
     )
 }
 
