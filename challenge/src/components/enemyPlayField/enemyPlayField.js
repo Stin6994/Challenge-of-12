868497bd418c4abadc1d2c0ae8e8be6387413877
Stin9469 <Stinger1994@mail.ru>
2025-07-29 10:54:52 +0300

    Стилизовано поле соперника

--- challenge/src/components/enemyPlayField/enemyPlayField.js
+++ challenge/src/components/enemyPlayField/enemyPlayField.js
@@ -1,4 +1,4 @@
-
+/* 
 import './enemyPlayField.css';
 import enemyFieldImg from '../../resources/img/playField.png';
 
@@ -28,5 +28,34 @@ function EnemyPlayField({ arr }) {
     );
 }
 
+
+export default EnemyPlayField;
+ */
+
+import './enemyPlayField.css';
+import enemyCardImg from '../../resources/img/enemyCard.png';
+import emptyImg from '../../resources/img/neon3.png';
+
+const EnemyPlayField = ({ arr }) => {
+    return (
+        <div className="enemy-cards-container">
+            <div className="enemy-cards-grid">
+                {arr.map((card, index) => (
+                    <div key={index} className="enemy-card-slot">
+                        {card === emptyImg ? (
+                            <div className="empty-card">
+                                <img src={emptyImg} alt="empty" />
+                            </div>
+                        ) : (
+                            <div className="enemy-card">
+                                <img src={enemyCardImg} alt="card back" />
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default EnemyPlayField;
