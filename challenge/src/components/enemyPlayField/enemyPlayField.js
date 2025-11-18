f1c48755b7cfc0942cd4a2a3dea9d89641f778f1
Stin9469 <Stinger1994@mail.ru>
2025-11-18 11:31:57 +0300

    Поле противника позиционировано

--- challenge/src/components/enemyPlayField/enemyPlayField.js
+++ challenge/src/components/enemyPlayField/enemyPlayField.js
@@ -1,36 +1,3 @@
-/* 
-import './enemyPlayField.css';
-import enemyFieldImg from '../../resources/img/playField.png';
-
-
-function EnemyPlayField({ arr }) {
-
-    return (
-
-        <div className='enemyFieldAll'>
-            <img src={enemyFieldImg}
-                className='enemyFieldImg'
-                alt="enemyPlayField" />
-
-
-            <div className="enemyFieldContent">
-                {arr.map((src, index) => (
-                    <img
-                        key={index}
-                        src={src}
-                        className='enemyCard'
-                        alt="enemyCard"
-                    />
-                ))}
-            </div>
-        </div>
-
-    );
-}
-
-
-export default EnemyPlayField;
- */
 
 import './enemyPlayField.css';
 import enemyCardImg from '../../resources/img/enemyCard.png';
