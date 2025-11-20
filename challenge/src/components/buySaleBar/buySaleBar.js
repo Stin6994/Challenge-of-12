6ec2a0ffb0caedac1ae5b37cb7aaec05b2768f40
Stin9469 <Stinger1994@mail.ru>
2025-11-20 13:07:31 +0300

    Меню покупки/продажи полностью готово

--- challenge/src/components/buySaleBar/buySaleBar.js
+++ challenge/src/components/buySaleBar/buySaleBar.js
@@ -36,46 +36,71 @@ function BuySaleBar({ myScore, setMyScore, result, setResult, bonus, setBonus, l
 
     return (
         <div className='buy-sale-container'>
-            <div className="buy-sale-score-display">
-                <span className="neon-text">Очки: {myScore.toLocaleString()}</span>
+
+            <div className="disp-cont">
+                <div className="score-display-bar">
+                    <div className="name-text-bar-cont">
+                        <span className="name-text-bar">Счет</span>
+                    </div>
+                    <div className="value-text-bar-cont">
+                        <span className="value-text-bar">{myScore.toLocaleString('ru-RU')}</span>
+                    </div>
+                </div>
+
+                <div className="score-display-bar">
+                    <div className="name-text-bar-cont-x">
+                        <span className="name-text-bar">Множитель</span>
+                    </div>
+                    <div className="value-text-bar-cont-x">
+                        <span className="value-text-bar">{bonus}</span>
+                    </div>
+
+                </div>
             </div>
-            
-            <div className="buy-sale-score-display">
-                <span className="neon-text">Множитель: Х{bonus}</span>
+
+            <div className="button-bar-cont">
+
+                <div className="tooltip-container">
+                    <button
+                        onClick={buyLife}
+                        disabled={life >= 3 || myScore < lifePrice}
+                        className="neon-button buy-button"
+                    /*  title={
+                         life >= 3
+                             ? "Максимальное количество жизней (3)"
+                             : myScore < lifePrice
+                                 ? `Не хватает очков (нужно ${lifePrice})`
+                                 : `Купить 1 жизнь за ${lifePrice} очков`
+                     } */
+                    >
+                        Купить за {lifePrice.toLocaleString('ru-RU')}
+                    </button>
+                    <span className="tooltip-text">{
+                         life >= 3
+                             ? "Максимальное количество жизней (3)"
+                             : myScore < lifePrice
+                                 ? `Не хватает очков (нужно ${lifePrice.toLocaleString('ru-RU')})`
+                                 : `Купить 1 жизнь за ${lifePrice.toLocaleString('ru-RU')} очков`
+                     }</span>
+                </div>
+
+                <div className="tooltip-container">
+                    <button
+                        onClick={sellLife}
+                        disabled={life <= 1}
+                        className="neon-button sell-button"
+                        title={life <= 1 ? "Нельзя продать последнюю жизнь" : `Продать 1 жизнь за ${(bonus * 1000)} очков`}
+                    >
+                        Продать за {(bonus * 1000).toLocaleString('ru-RU')}
+                    </button>
+                    <span className="tooltip-text">{life <= 1 ? "Нельзя продать последнюю жизнь" : `Продать 1 жизнь за ${(bonus * 1000).toLocaleString('ru-RU')} очков`}</span>
+                </div>
             </div>
-            
-            <button
-                onClick={buyLife}
-                disabled={life >= 3 || myScore < lifePrice}
-                className="neon-button buy-button"
-                title={
-                    life >= 3
-                        ? "Максимальное количество жизней (3)"
-                        : myScore < lifePrice
-                            ? `Не хватает очков (нужно ${lifePrice.toLocaleString()})`
-                            : `Купить 1 жизнь за ${lifePrice.toLocaleString()} очков`
-                }
-            >
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                Купить за {lifePrice.toLocaleString()}
-            </button>
-            
-            <button
-                onClick={sellLife.toLocaleString()}
-                disabled={life <= 1}
-                className="neon-button sell-button"
-                title={life <= 1 ? "Нельзя продать последнюю жизнь" : `Продать 1 жизнь за ${(bonus * 1000).toLocaleString()} очков`}
-            >
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                Продать за {(bonus * 1000).toLocaleString()}
-            </button>
+
+
         </div>
+
+
     );
 }
 
